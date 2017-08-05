46b5db5119ccfbb60308c981df7c1d267bd42623
Samuel Van Zuijlen <sam.vzh@orange.fr>
2017-08-05 11:57:56 +0200

    Ajout de l'entete # -*-coding:utf-8 -* en haut des fichiers

--- arbre_competences/urls.py
+++ arbre_competences/urls.py
@@ -1,3 +1,4 @@
+# -*-coding:utf-8 -*
 """arbre_competences URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
--- arbre_competences/wsgi.py
+++ arbre_competences/wsgi.py
@@ -1,3 +1,4 @@
+# -*-coding:utf-8 -*
 """
 WSGI config for arbre_competences project.
 
