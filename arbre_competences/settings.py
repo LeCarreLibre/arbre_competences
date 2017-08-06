6fa74aaf45b69e8a47732ef5c1857a76eccd4ce7
Samuel Van Zuijlen <sam.vzh@orange.fr>
2017-08-06 21:27:18 +0200

    Setings for mysql

--- arbre_competences/settings.py
+++ arbre_competences/settings.py
@@ -1,3 +1,4 @@
+# -*-coding:utf-8 -*-
 """
 Django settings for arbre_competences project.
 
@@ -76,23 +77,23 @@ WSGI_APPLICATION = 'arbre_competences.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': 'arbre_competences',
-#        'USER': 'test',
-#        'PASSWORD': 'test',
-#        'HOST': '127.0.0.1',
-#        'PORT': '',
+#     'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #    }
 #}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'arbre_competences',
+        'USER': 'test',
+        'PASSWORD': 'test',
+        'HOST': '127.0.0.1',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
