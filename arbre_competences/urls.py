ea468a115769b0fbb5cf6aa07944f80e8033e783
Samuel Van Zuijlen <sam.vzh@orange.fr>
2017-09-05 10:42:36 +0200

    Rename app to competences

--- arbre_competences/urls.py
+++ arbre_competences/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from app import urls
+from competences import urls
 
 urlpatterns = [
     url(r'^admin/?', admin.site.urls),
