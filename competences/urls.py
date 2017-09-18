4ac7dbfa1382a87e78460183c709eb076ba56dca
Samuel <sam.vzh@orange.fr>
2017-09-18 15:00:59 +0200

    Add an aplication urls.py

---
+++ competences/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from competences.views import ListeProfils, AffichageProfil
+
+urlpatterns = [
+	url(r'^profils/$', ListeProfils.as_view(), name="liste_profils"),
+	url(r'^profil/(?P<pk>\d+)', AffichageProfil.as_view(),
+		name="affichage_profil"),
+]
