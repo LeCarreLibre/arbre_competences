357915c8c64d4d628caf49c34d2a9bf4d6a24f23
Samuel <sam.vzh@orange.fr>
2017-12-04 21:50:31 +0100

    Add a debug toolbar

--- arbre_competences/urls.py
+++ arbre_competences/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 Ce programme est sous licence GNU GPL
 ©2017 Nils et Samuel Van Zuijlen
 """
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
 
@@ -25,5 +26,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^', include('competences.urls')),
-    url(r'^competences/', include('competences.urls')),
 ]
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
