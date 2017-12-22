92eec9469ceb9acc4378df394c36fcd172def4ea
Samuel <sam.vzh@orange.fr>
2017-12-22 09:57:54 +0100

    Traduction en fr du formulaire

--- competences/forms.py
+++ competences/forms.py
@@ -4,11 +4,11 @@ from django import forms
 class AddUserForm(forms.Form):
     """create a user with a simple way"""
 
-    lastname = forms.CharField(max_length=100)
-    firstname = forms.CharField(max_length=100)
-    username = forms.CharField(max_length=100)
+    lastname = forms.CharField(max_length=100, label="Nom")
+    firstname = forms.CharField(max_length=100, label="Prénom")
+    username = forms.CharField(max_length=100, label="Nom d'utilisateur")
     email = forms.EmailField(label="Votre adresse mail")
     telephone = forms.CharField(min_length=10, max_length=15)
     voluntary = forms.BooleanField(help_text="Cochez si vous souhaitez être "
-                                             "bénévole.", required=False)
-    addresses = forms.CharField()
+                                             "bénévole.", label="Bénevole", required=False)
+    addresses = forms.CharField(label="Adresse")
