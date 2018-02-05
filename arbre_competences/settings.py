1b77b72c4acc3262886cabdc34db9b4ea1a72550
Nils Van Zuijlen <nilsdu29@gmail.com>
2018-02-05 23:15:06 +0100

    Simplified and made nicer.

--- arbre_competences/settings.py
+++ arbre_competences/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'competences',
     'debug_toolbar',
 ]
